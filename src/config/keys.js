05d5fb93846c05abf5790e471470732eceef0ae5
Keeano <keeano92@gmail.com>
2019-11-19 12:10:05 -0500

    adding onesignal keys

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
-    mongoURI: "Your Mongo URI",
-    userAuthKey: "OneSingal Auth Key",
-    appAuthKey: "YOneSignal App Key",
-    appId: "One Signal App Id",
+    mongoURI: "mongodb+srv://admin:6JsteBFmfKQUcqq0@cluster0-gaygc.mongodb.net/farmestar?retryWrites=true&w=majority",
+    userAuthKey: "MTZlNWI0ZGQtODVmNC00NDliLTk3ODYtOTFkODE0NmUxYmQz",
+    appAuthKey: "NDYyMDU0YmEtOGM5OS00YTQ2LTgxY2UtZGRjNTI5ZDI4OTgw",
+    appId: "61035d16-4f24-4696-8645-cf5c54e20787",
     secretOrKey: "secret"
 }

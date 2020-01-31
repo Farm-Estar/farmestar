1ab69174a18c51f025be7fd683aec0c6f42783c7
Keeano <keeano92@gmail.com>
2020-01-31 17:26:52 -0500

    Updating DB mapping to not be in test anymore

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb+srv://admin:6JsteBFmfKQUcqq0@farmestardev-gaygc.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://admin:6JsteBFmfKQUcqq0@farmestardev-gaygc.mongodb.net/farmestar?retryWrites=true&w=majority",
     userAuthKey: "MTZlNWI0ZGQtODVmNC00NDliLTk3ODYtOTFkODE0NmUxYmQz",
     appAuthKey: "NDYyMDU0YmEtOGM5OS00YTQ2LTgxY2UtZGRjNTI5ZDI4OTgw",
     appId: "61035d16-4f24-4696-8645-cf5c54e20787",
